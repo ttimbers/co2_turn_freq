076d53bf9d828519812901774bc6a361ff83e773
ttimbers <tiffany.timbers@gmail.com>
2016-10-16 14:36:01 -0700

    Added feature to plot turns/minute

--- src/analyze_turns.R
+++ src/analyze_turns.R
@@ -4,6 +4,7 @@
 # load libraries
 library(tidyverse)
 library(peakPick)
+library(stringr)
 
 # get command args
 args <- commandArgs(trailingOnly = TRUE)
@@ -22,24 +23,61 @@ main <- function(){
   turns_data$aspect_ratio <- turns_data$morphwidth / turns_data$midline 
   
   # Detect Peaks of M/m (min_peak_height = 0.25, min_peak_distance = 90)
-   
-  # split by plate, & id  
   turns_data <- turns_data %>% 
-    #filter(strain == "N2", plate == "20130524_140719") %>% 
     group_by(plate, id) %>% 
     nest() %>% 
     mutate(turns = map(data, detect_HA_turns)) %>% 
     filter(!is.na(turns)) %>% 
-    select(turns) %>% 
+    select(turns, id, plate) %>% 
     unnest()
-
-  # make tidy data frame to combine size_data & good_number_data
   
+  
+  # Determine the number of turns/minute per worm 
+  
+  # calculate mean N at each time interval
+  time_interval <- seq(from = 0, to = 300, by = 20)
+  
+  # calculate turns per per plate per time bin
+  turns_by_interval <- turns_data %>% 
+    mutate(interval = cut(time, time_interval)) %>% 
+    group_by(plate, strain, interval) %>% 
+    summarise(turns = n())
+  
+  # calculate good number per plate per time bin
+  good_number_by_interval <- good_number_data %>% 
+    mutate(interval = cut(time, time_interval)) %>% 
+    group_by(plate, strain, interval) %>% 
+    summarise(count = round(mean(good_number)))
+  
+  # create dataframe to get turns/min for each time bin
+  turns_min <- left_join(good_number_by_interval, turns_by_interval)
+  turns_min$turns[is.na(turns_min$turns)] <- 0
+  turns_min$tpm <- (turns_min$turns / turns_min$count) * 3
+  
+  # create summarised data frame to plot mean turns/min for each time bin
+  turns_min <- turns_min %>% 
+    group_by(strain, interval) %>% 
+    summarise(mean_tpm = median(tpm),
+              sd_tpm = sd(tpm),
+              se_tpm = sd(tpm) / sqrt(n()),
+              ci_tpm = (sd(tpm) / sqrt(n()) * 1.96)) 
+  
+  turns_min$interval <- as.numeric(str_extract(turns_min$interval, "[1-9]{1}[0-9]+"))
+  turns_min$interval[is.na(turns_min$interval)] <- 300
+  turns_min$interval <- turns_min$interval + 10 #set to mid of time bin
   
   # make vis and write to file
+  ha_plot <- ggplot(turns_min, aes(y = mean_tpm, x = interval, colour = strain)) + 
+    geom_errorbar(aes(ymin = mean_tpm - ci_tpm, ymax = mean_tpm + ci_tpm)) +
+    geom_line(aes(group = strain)) + 
+    geom_point() +
+    labs(x="Time", y="High amplitude turns/minute") 
+  
+  output_ha_plot_name <- paste0(output_file_prefix, "_ha_plot.pdf")
+  ggsave(output_ha_plot_name, ha_plot, height = 3, width = 4)
   
   # do stats and write to file
-
+  
   
 }
 
@@ -50,20 +88,20 @@ main <- function(){
 #   min_peak_height = minimum aspect ratio to be considered a HA turn (default = 0.25)
 #   min_peak_distance = minimum peak distance (in sec) between turns (default = 90)
 #
-# Returns: a data frame containing only the records where peaks occurred
+# Returns: a data frame containing only the records where peaks occurred, if there was 
+#          no peak, it returns NA
 detect_HA_turns <- function(x, min_peak_height = 0.25, min_peak_distance = 90) {
   
-  if (max(x$aspect_ratio > 0.25)) { 
+  if (max(x$aspect_ratio > min_peak_height)) { 
     x_ts <- as.matrix(select(x, time, aspect_ratio))
     # neighlim = (min_peak_distance * 25) because neighlim is integer val, and camera
     # records at ~ 25 frames/sec
     peaks_bool <- peakpick(x_ts, neighlim = (min_peak_distance * 25), peak.npos = 50)
   } else {
-      return (NA) # if this fails, try returning an empty tibble via tibble()
+      return (NA)
     }
 
   turns <- filter(x, peaks_bool[,2], aspect_ratio > min_peak_height)
-  
   return(turns)
 }
 
